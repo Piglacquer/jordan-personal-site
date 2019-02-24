bce2e3c57581e0e39ebb7499317efe08405c9b3f
Patrick Biffle <pbiffle@gmail.com>
2019-02-23 21:13:08 -0700

    initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class='nav'>
+    <div>
+      <router-link class='link' to='/'>Home</router-link>
+    </div>
+    <div class='links'>
+      <router-link class='link' to='/work'>Work</router-link>
+      <router-link class='link' to='/about'>About</router-link>
+      <router-link class='link' to='/contact'>Contact</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'navBar'
+}
+</script>
+
+<style scoped>
+  .nav {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    align-items: center;
+    height: 10vh;
+    margin: 0 20px 0 20px;
+  }
+
+  .links {
+    width: 25vw;
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .link {
+    text-decoration: none;
+    color: white;
+  }
+  .link:visted {
+    color: white;
+  }
+</style>
---
+++ src/views/Contact.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="contact">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'contact',
+  components: {
+    
+  }
+}
+</script>
